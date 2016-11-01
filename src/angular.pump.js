934371d0cf9c41407822aa239b96cea1027ac7fb
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2016-11-01 17:31:24 +0300

    equals: patch

--- src/angular.pump.js
+++ src/angular.pump.js
@@ -1076,44 +1076,49 @@ function copy(source, destination) {
   </example>
  */
 function equals(o1, o2) {
-  if (o1 === o2) return true;
-  if (o1 === null || o2 === null) return false;
-  if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
-  if (typeof o1 === 'object' && typeof o2 === 'object' ) {
-    var length, key, keySet
-    if (isArray(o1)) {
-      if (!isArray(o2)) return false;
-      if ((length = o1.length) == o2.length) {
-        for (key = 0; key < length; key++) {
-          if (!equals(o1[key], o2[key])) return false;
-        }
-        return true;
+   if (o1 === o2) return true;
+   if (o1 === null || o2 === null) return false;
+   if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
+   if (typeof o1 === 'object' && typeof o2 === 'object') {
+      var t1 = toString.call(o1);
+      var length, key, keySet
+      if (t1 === '[object Array]') {
+         if (toString.call(o2) !== '[object Array]') return false;
+         if ((length = o1.length) === o2.length) {
+            for (key = 0; key < length; key++) {
+               if (!equals(o1[key], o2[key])) return false;
+            }
+            return true;
+         }
+      } else if (t1 === '[object Date]') {
+         if (toString.call(o2) !== '[object Date]') return false;
+         return equals(o1.getTime(), o2.getTime());
+      } else if (t1 === '[object RegExp]') {
+         if (toString.call(o2) !== '[object RegExp]') return false;
+         return o1.toString() === o2.toString();
+      } else {
+         if (isScope(o1) || isScope(o2) || isWindow(o1) || isWindow(o2) ||
+             isArray(o2) || isDate(o2) || isRegExp(o2)) return false;
+         keySet = Object.create(null);
+         for (key in o1) {
+            if (key.charAt(0) === '$') continue;
+            var v = o1[key];
+
+            if (isFunction(v)) continue;
+            if (!equals(v, o2[key])) return false;
+            keySet[key] = true;
+         }
+         for (key in o2) {
+            if (!(key in keySet) &&
+                key.charAt(0) !== '$') {
+               var v = o2[key];
+               if(isDefined(v) && !isFunction(v)) return false;
+            }
+         }
+         return true;
       }
-    } else if (isDate(o1)) {
-      if (!isDate(o2)) return false;
-      return equals(o1.getTime(), o2.getTime());
-    } else if (isRegExp(o1)) {
-      if (!isRegExp(o2)) return false;
-      return o1.toString() == o2.toString();
-    } else {
-      if (isScope(o1) || isScope(o2) || isWindow(o1) || isWindow(o2) ||
-        isArray(o2) || isDate(o2) || isRegExp(o2)) return false;
-      keySet = createMap();
-      for (key in o1) {
-        if (key.charAt(0) === '$' || isFunction(o1[key])) continue;
-        if (!equals(o1[key], o2[key])) return false;
-        keySet[key] = true;
-      }
-      for (key in o2) {
-        if (!(key in keySet) &&
-            key.charAt(0) !== '$' &&
-            isDefined(o2[key]) &&
-            !isFunction(o2[key])) return false;
-      }
-      return true;
-    }
-  }
-  return false;
+   }
+   return false;
 }
 
 var csp = function() {
