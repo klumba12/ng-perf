100ebe2af2908fefe9c35c96134bae380f8be3e5
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2016-11-02 14:39:07 +0300

    Use patched equals instead of ===; peStyle perfomance improvements;

--- src/pe.js
+++ src/pe.js
@@ -1,4 +1,4 @@
-(function (angular) {
+(function (angular, undefined) {
 
    angular.module('pe', [])
        .service('pe', pe)
@@ -32,10 +32,119 @@
       }
    }
 
+   var isArray = angular.isArray,
+       isDate = angular.isDate,
+       isFunction = angular.isFunction,
+       isDefined = angular.isDefined,
+       toString = Object.prototype.toString;
+
+   function isRegExp(value) {
+      return toString.call(value) === '[object RegExp]';
+   }
+
+   function isWindow(obj) {
+      return obj && obj.window === obj;
+   }
+
+   function isScope(obj) {
+      return obj && obj.$evalAsync && obj.$watch;
+   }
+
+   function equals(o1, o2) {
+      if (o1 === o2) return true;
+      if (o1 === null || o2 === null) return false;
+      if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
+      if (typeof o1 === 'object' && typeof o2 === 'object') {
+         var t1 = toString.call(o1);
+         var length, key, keySet
+         if (t1 === '[object Array]') {
+            if (toString.call(o2) !== '[object Array]') return false;
+            if ((length = o1.length) === o2.length) {
+               for (key = 0; key < length; key++) {
+                  if (!equals(o1[key], o2[key])) return false;
+               }
+               return true;
+            }
+         } else if (t1 === '[object Date]') {
+            if (toString.call(o2) !== '[object Date]') return false;
+            return equals(o1.getTime(), o2.getTime());
+         } else if (t1 === '[object RegExp]') {
+            if (toString.call(o2) !== '[object RegExp]') return false;
+            return o1.toString() === o2.toString();
+         } else {
+            if (isScope(o1) || isScope(o2) || isWindow(o1) || isWindow(o2) ||
+                isArray(o2) || isDate(o2) || isRegExp(o2)) return false;
+            keySet = Object.create(null);
+            for (key in o1) {
+               if (key.charAt(0) === '$') continue;
+               var v = o1[key];
+
+               if (isFunction(v)) continue;
+               if (!equals(v, o2[key])) return false;
+               keySet[key] = true;
+            }
+            for (key in o2) {
+               if (!(key in keySet) &&
+                   key.charAt(0) !== '$') {
+                  var v = o2[key];
+                  if (isDefined(v) && !isFunction(v)) return false;
+               }
+            }
+            return true;
+         }
+      }
+      return false;
+   }
+
+   function camelCase(name) {
+      var length = name.length,
+          result = '',
+          capitalize = false;
+
+      for (var i = 0; i < length; i++) {
+         var c = name.charAt(i);
+         if (c === ':' || c === '-' || c === '_') {
+            capitalize = true;
+            continue;
+         }
+
+         if (capitalize) {
+            result += c.toUpperCase();
+            capitalize = false;
+         }
+         else {
+            result += c;
+         }
+      }
+
+      if (result.charAt(0) === 'm' &&
+          result.charAt(1) === 'o' &&
+          result.charAt(2) === 'z') {
+         var n = result.charAt(3);
+         if (n && n.toUpperCase() === n) {
+            result[0] = 'M';
+         }
+      }
+
+
+      return result;
+   }
+
+   function style(element, name, value) {
+      if (angular.isObject(value)) {
+         for (var key in value) {
+            element.style[key] = value[key];
+         }
+      }
+      else {
+         element.style[name] = value;
+      }
+   }
+
    function pe($rootScope, $parse) {
       var watchers = [],
           isDirty = false;
-          //digestQueue = [];
+      //digestQueue = [];
 
       function notify(e) {
          isDirty = true;
@@ -51,7 +160,7 @@
                 newValue = w.get(),
                 oldValue = w.value;
 
-            if (newValue !== oldValue) {
+            if (!equals(newValue, oldValue)) {
                w.value = newValue;
                w.handler(newValue, oldValue);
             }
@@ -70,7 +179,7 @@
             var get = $parse(expression),
                 value = get(scope),
                 w = {
-                   get: function(){
+                   get: function () {
                       return get(scope);
                    },
                    handler: handler,
@@ -108,15 +217,25 @@
       return {
          restrict: 'A',
          link: function (scope, element, attr) {
-            var watch = pe.watch(scope);
+            var watch = pe.watch(scope),
+                node = element[0],
+                convertToCamelCase = !!attr.peCamelCase;
+
             watch(attr.peStyle, function ngStyleWatchAction(newStyles, oldStyles) {
                if (oldStyles && (newStyles !== oldStyles)) {
-                  angular.forEach(oldStyles, function (val, style) {
-                     element.css(style, '');
-                  });
+                  for (var key in oldStyles) {
+                     var name = convertToCamelCase ? camelCase(key) : key;
+                     node.style[name] = '';
+                  }
                }
-               if (newStyles) element.css(newStyles);
-            }, true);
+
+               if (newStyles) {
+                  for (var key in newStyles) {
+                     var name = convertToCamelCase ? camelCase(key) : key;
+                     node.style[name] = newStyles[key];
+                  }
+               }
+            });
          }
       };
    }
